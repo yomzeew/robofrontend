6cdd6d0e6b4c8e8dc165a47f7c8f2978ccbf5a8d
yomzeew <yomzeew@gmail.com>
2023-11-05 21:26:22 +0100

    update footer

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -40,7 +40,7 @@ const Footer=()=>{
 
                             Blog<br></br>
                             Help Center<br></br>
-                            What level of risk suits you best?<br></br>
+                            What level of risk?<br></br>
                             Home Planning Guide<br></br>
                             Financial Health Guide<br></br>
                             Equity and IPO Guide<br></br>
