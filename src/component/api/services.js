5eac63fb6adf6ef643bd1402a4b340eafb44289d
yomzeew <yomzeew@gmail.com>
2023-11-05 06:30:50 +0100

    mywork

--- src/component/api/services.js
+++ src/component/api/services.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-  baseURL: 'http://13.48.138.21:3004', // Replace with your API base URL
+  baseURL: 'https://roboadvisor-70d5c6f14585.herokuapp.com', // Replace with your API base URL
 });
 
 export default api;
