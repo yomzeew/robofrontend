ec0e2f8198f910d7da0f11d123dda1c0ecd88608
Babayomi Oluwasuyi <61556760+yomzeew@users.noreply.github.com>
2023-11-05 10:42:32 +0100

    Update services.js

--- src/component/api/services.js
+++ src/component/api/services.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-  baseURL: 'https://roboadvisor-70d5c6f14585.herokuapp.com', // Replace with your API base URL
+  baseURL: 'http://ec2-13-48-138-21.eu-north-1.compute.amazonaws.com:3004', // Replace with your API base URL
 });
 
 export default api;
